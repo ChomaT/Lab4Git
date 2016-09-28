c37e033f90794c0d67c69225ede5f26d546d46e5
ChomaT <theochoma@gmail.com>
2016-09-28 12:09:49 -0500

    Commit from Home
    Compiled, but will not run on home computer for some reason.

--- src/Lab4.c
+++ src/Lab4.c
@@ -12,6 +12,8 @@
 #include <stdlib.h>
 
 void arrayops();
+double average(int numdata, int* array);
+int maximum(int numdata, int* array);
 
 
 int main(void) {
@@ -59,6 +61,77 @@ void arrayops()
 
 
 
+        //!!!!!PART 2!!!!!
+        double avg;
+        int max;
+        
+        //Getting stats
+        avg = average(numdata, array);
+        max = maximum(numdata, array);
+        
+        //Saving stats
+        sprintf(outstr, "Statistics_data_%d.txt", numfile);
+        fp2 = fopen(outstr, "w");
+        if(fp2 == NULL)
+        {
+                perror("\n!!!Error opening writing file!!!\n");
+        }
+        else
+        {
+                fprintf(fp2, "%lf %d\n", avg, max);
+        }
+        fclose(fp2);
+        
+        
+        //Centering
+        for(i = 0; i < numdata; i++)
+        {
+                outarray[i] = (double)array[i] - avg;
+        }
+        
+        //Saving Centering
+        sprintf(outstr, "Centered_data_%d.txt", numfile);
+        fp2 = fopen(outstr, "w");
+        if(fp2 == NULL)
+        {
+                perror("\n!!!Error opening writing file!!!\n");
+        }
+        else
+        {
+                fprintf(fp2, "%d %lf\n", numdata, avg);
+                for(i = 0; i < numdata; i++)
+                {
+                        fprintf(fp2, "%lf\n", outarray[i]);
+                }
+        }
+        fclose(fp2);
+        
+        
+        //Normalizing
+        for(i = 0; i < numdata; i++)
+        {
+                outarray[i] = (double)array[i] / (double) max;
+        }
+
+        //saving Normal
+        sprintf(outstr, "Normalized_data_%d.txt", numfile);
+        fp2 = fopen(outstr, "w");
+        if(fp2 == NULL)
+        {
+                perror("\n!!!Error opening writing file!!!\n");
+        }
+        else
+        {
+                fprintf(fp2, "%d %d\n", numdata, max);
+                for(i = 0; i < numdata; i++)
+                {
+                        fprintf(fp2, "%lf\n", outarray[i]);
+                }
+        }
+        fclose(fp2);
+        
+        //!!!!!END OF PART 2!!!!!
+        
 	//array transformation
 	printf("\nWould you like to offset (1) or scale (2) your data? ");
 		fflush(stdout);
@@ -124,4 +197,33 @@ void arrayops()
 		fclose(fp2);
 	}
 
+}
+
+double average(int numdata, int* array)
+{
+    double out = 0;
+    int i = 0;
+    
+    for(i = 0; i < numdata; i++)
+    {
+        out = out + (double)array[i];
+    }
+    out = out / (double)numdata;
+    
+    return out;
+    
+}
+
+int maximum(int numdata, int* array)
+{
+    int out = array[0];
+    int i = 0;
+    for(i = 0; i < numdata; i++)
+    {
+        if(out < array[i])
+            out = array[i];
+    }
+    
+    return out;
+    
 }
